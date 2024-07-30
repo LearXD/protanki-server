08b084a7f9d12d2bdd21fa6b6cf5de51a9281042
LearXD <gutyxdcanal@gmail.com>
2024-07-30 16:01:41 -0300

    fix: fixed start

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
 if [ ! -d "node_modules" ]; then
   echo "node_modules não encontrado, instalando dependências..."
   npm install
 fi
+
+if [ ! -d "dist" ]; then
+  echo "binário do servidor não encontrado, compilando..."
+  npm run build
+fi
+
 npm start
